8bbabafad6f0ba9b1927cdc6166a30c6035a7ae1
Andrew <radnen@gmail.com>
2013-07-05 14:30:36 -0700

    Added Exit

--- Engine/Engine/Program.cs
+++ Engine/Engine/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using SFML;
+using SFML.Graphics;
 using SFML.Window;
 using Noesis.Javascript;
 
@@ -8,49 +9,59 @@ namespace Engine
 {
     static class Program
     {
-        static Window window = null;
-        static JavascriptContext ctxt = null;
+        static RenderWindow _window = null;
+        static JavascriptContext _ctxt = null;
 
         static void Main()
         {
-            window = new Window(new VideoMode(640, 480, 32), "Game", Styles.Titlebar | Styles.Close, new ContextSettings(32, 0));
-            window.SetVerticalSyncEnabled(true);
+            _window = new RenderWindow(new VideoMode(640, 480, 32), "Game", Styles.Titlebar | Styles.Close, new ContextSettings(32, 0));
+			_window.SetVerticalSyncEnabled(true);
 
-            ctxt = new JavascriptContext();
-            ctxt.SetParameter("FlipScreen", new Action(FlipScreen));
-            ctxt.SetParameter("RequireScript", new Action<string>(RequireScript));
-            ctxt.SetParameter("Print", new Action<string>(Print));
+            _ctxt = new JavascriptContext();
+			_ctxt.SetParameter("FlipScreen", new Action(FlipScreen));
+			_ctxt.SetParameter("RequireScript", new Action<string>(RequireScript));
+			_ctxt.SetParameter("Print", new Action<string>(Print));
+			_ctxt.SetParameter("Exit", new Action(Exit));
+			_ctxt.SetParameter("LoadImage", new Func<string, object>(LoadImage));
 
             string code = ReadCodeFile("test.js"); // TODO: read this in from a .sgm
             code += "game();";
 
-            window.Closed += window_Closed;
+            _window.Closed += window_Closed;
 
-            if (window.IsOpen()) { RunCode(code); }
+            if (_window.IsOpen()) { RunCode(code); }
         }
 
         // TODO: make this not assume 'Startup' dir.
         static string ReadCodeFile(string filename)
-        {
-            StreamReader reader = null;
-            string code = "";
+		{
+			StreamReader reader = null;
+			string code = "";
 
-            try
-            {
-                reader = new StreamReader("Startup/scripts/" + filename);
-            }
-            catch (Exception e) { Console.WriteLine("Error: could not load file " + filename + " - (" + e.Message + ")"); }
-            finally
-            {
-                if (reader != null)
-                {
-                    code = reader.ReadToEnd();
-                    Console.WriteLine("Success: read in " + filename);
-                }
-            }
+			try
+			{
+				reader = new StreamReader(GlobalProps.BasePath + "/scripts/" + filename);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error: could not load file " + filename + " - (" + e.Message + ")");
+			}
+			finally
+			{
+				if (reader != null)
+				{
+					code = reader.ReadToEnd();
+					Console.WriteLine("Success: read in " + filename);
+				}
+			}
 
-            return code;
-        }
+			return code;
+		}
+
+		static void Exit()
+		{
+			_window.Close();
+		}
 
         static void Print(object obj)
         {
@@ -59,21 +70,26 @@ namespace Engine
 
         static void FlipScreen()
         {
-            window.Display();
-            window.DispatchEvents();
+            _window.Display();
+            _window.DispatchEvents();
+        }
+
+        static object LoadImage(string filename)
+        {
+			return new Engine.Objects.ImageWrapper (filename, _window);
         }
 
         static void RunCode(string code)
-        {
-            try
-            {
-                ctxt.Run(code);
-            }
-            catch (JavascriptException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+		{
+			try
+			{
+				_ctxt.Run(code);
+			}
+			catch (JavascriptException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
 
         static void RequireScript(string filename)
         {
@@ -82,14 +98,12 @@ namespace Engine
         }
 
         static void window_Closed(object sender, EventArgs e)
-        {
-            Window window = sender as Window;
-            if (window != null)
-            {
-                window.Close();
-            }
-        }
-
-
+		{
+			Window window = sender as Window;
+			if (window != null)
+			{
+				window.Close();
+			}
+		}
     }
 }
