7b1a73a17ecab5299cd676d3e3233158511c4ebf
Andrew <radnen@gmail.com>
2013-07-05 00:55:19 -0700

    Initial commit
    The code.

---
+++ Engine/Engine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFML;
+using SFML.Window;
+
+namespace Engine
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Window window = new Window(new VideoMode(640, 480, 32), "Game", Styles.Titlebar | Styles.Close, new ContextSettings(32, 0));
+            window.SetVerticalSyncEnabled(true);
+
+            window.Closed += window_Closed;
+
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();
+                window.Display();
+            }
+        }
+
+        static void window_Closed(object sender, EventArgs e)
+        {
+            Window window = sender as Window;
+            if (window != null) window.Close();
+        }
+    }
+}
---
+++ Engine/Engine/Startup/scripts/test.js
@@ -0,0 +1,6 @@
+function game()
+{
+	while (true) {
+		FlipScreen();
+	}
+}
